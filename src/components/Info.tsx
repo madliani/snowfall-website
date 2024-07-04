5dec827015387b8823b0cdc386255a571dc9199d
madliani <madliani@hotmail.com>
2024-07-04 17:10:19 +0400

    fix: info fixed

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -3,37 +3,32 @@ import styled from "styled-components";
 
 type Properties = {
     children: ReactNode;
+    subtitle: string;
+    title: string;
 };
-
-const Container = styled.div`
-    position: relative;
-    height: 80vh;
-`;
-
-const Jumbotron = styled.div`
-    position: absolute;
-
-    width: 100%;
-    margin: 0;
-    padding: 2rem 1rem;
-
-    background: none;
-
-    @media (max-width: 768px) {
-        top: 12vh;
-    }
-`;
 
 const Text = styled.div`
     line-height: 2rem;
 `;
 
-export const Info = ({ children }: Properties) => {
+export const Info = ({ children, subtitle, title }: Properties) => {
     return (
-        <Container>
-            <Jumbotron className="jumbotron jumbotron-fluid">
-                <Text>{children}</Text>
-            </Jumbotron>
-        </Container>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="section-title">
+                        <span className="main-color">{title}</span>
+                    </h1>
+                    <h2 className="mb-5">
+                        <span>{subtitle}</span>
+                    </h2>
+                </div>
+            </div>
+            <div className="row text-start">
+                <div className="col-md-6 about-section">
+                    <Text>{children}</Text>
+                </div>
+            </div>
+        </div>
     );
 };

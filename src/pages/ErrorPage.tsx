c9b9e938ef3d3ecd0ce4c02ee95041e05c007d82
madliani <madliani@hotmail.com>
2024-07-04 17:13:53 +0400

    refactor: error page refactored

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -25,21 +25,17 @@ export const ErrorPage = () => {
         <>
             <SEO title="404: Not Found" />
             <Layout>
-                <div>
-                    <Jumbotron className="jumbotron-fluid error-jumbotron">
-                        <h1>
-                            <span className="main-color">404</span> Page Not
-                            Found
-                        </h1>
-                        <h6>
-                            Sorry but the page you are looking for is not
-                            available.
-                        </h6>
-                        <Link to="/">
-                            <button className="primary-btn">Return home</button>
-                        </Link>
-                    </Jumbotron>
-                </div>
+                <Jumbotron className="jumbotron-fluid error-jumbotron">
+                    <h1>
+                        <span className="main-color">404</span> Page Not Found
+                    </h1>
+                    <h6>
+                        Sorry but the page you are looking for is not available.
+                    </h6>
+                    <Link to="/">
+                        <button className="primary-btn">Return home</button>
+                    </Link>
+                </Jumbotron>
             </Layout>
         </>
     );
